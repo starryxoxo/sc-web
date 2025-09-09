d7de40b3f7f8be82482bd25bf904e7ab72c12e6c
starryxoxo <112181939+starryxoxo@users.noreply.github.com>
2025-09-09 22:55:27 +0800

    Create ghk.11ty.js

---
+++ src/ghk.11ty.js
@@ -0,0 +1,21 @@
+// ghk.11ty.js
+const users = require("./_data/user.json");
+
+module.exports = {
+  permalink: "/ghk.json",
+  eleventyExcludeFromCollections: true,
+  data: {
+    secret: "mySnOnbH9729"
+  },
+  render: function(data) {
+    const url = data.page.url || "";
+    const query = new URLSearchParams(url.split("?")[1]);
+    const key = query.get("key");
+
+    if (key === data.secret) {
+      return JSON.stringify(users.users, null, 2);
+    } else {
+      return JSON.stringify({ error: "Access denied" }, null, 2);
+    }
+  }
+};
