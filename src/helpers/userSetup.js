1d4fe63db7031c0dd3e19ee19f9256ce866371e8
starryxoxo <112181939+starryxoxo@users.noreply.github.com>
2025-09-10 00:33:47 +0800

    Update userSetup.js

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -6,7 +6,7 @@ function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
   // Feel free to add any plugin you want here instead of /.eleventy.js
     eleventyConfig.addPassthroughCopy("netlify/functions/*.js");
-  eleventyConfig.addPassthroughCopy("user.json");
+  eleventyConfig.addPassthroughCopy("ghk.json");
 }
 
 exports.userMarkdownSetup = userMarkdownSetup;
