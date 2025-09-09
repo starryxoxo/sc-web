1de1c639c284c96c779517f8e4e0a93789a8ba16
starryxoxo <112181939+starryxoxo@users.noreply.github.com>
2025-09-09 21:24:30 +0800

    Update userUtils.js

--- src/helpers/userUtils.js
+++ src/helpers/userUtils.js
@@ -5,11 +5,3 @@ function userComputed(data) {
 }
 
 exports.userComputed = userComputed;
-
-module.exports = function(eleventyConfig) {
-  return {
-    dir: {
-      output: "_site"  // or another folder name like "dist"
-    }
-  };
-};
