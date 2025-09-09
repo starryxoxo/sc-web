23729493f5c065743dafd2d0c8c18d6d4ece1ef5
starryxoxo <112181939+starryxoxo@users.noreply.github.com>
2025-09-09 21:29:16 +0800

    Update userSetup.js

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -9,12 +9,10 @@ function userEleventySetup(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("user.json");
 }
 
-module.exports = function(eleventyConfig) {
-  return {
-    dir: {
-      output: "_site"  // or another folder name like "dist"
-    }
-  };
+module.exports = {
+  dir: {
+    output: "_site"
+  }
 };
 
 exports.userMarkdownSetup = userMarkdownSetup;
